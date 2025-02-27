6dbfba16d99622c29a26328d428aa4c682b6e12a
Valentino <entinosusini@gmail.com>
2025-02-27 20:31:27 +0100

    Add files via upload

---
+++ Philo/philo.c
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 13:43:12 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/02/27 19:24:56 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	parse_args(t_data *data, int argc, char **argv)
+{
+	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2])
+		|| !is_valid_number(argv[3]) || !is_valid_number(argv[4]) || (argc == 6
+			&& !is_valid_number(argv[5])) || safe_atoi(argv[1]) < 1)
+		return (1);
+	if (safe_atoi(argv[1]) == 1)
+	{
+		printf("0 1 has taken a fork\n");
+		usleep(safe_atoi(argv[2]) * 1000);
+		return (printf("%d 1 has died\n", safe_atoi(argv[2])), 2);
+	}
+	data->num_philos = safe_atoi(argv[1]);
+	data->time_to_die = safe_atoi(argv[2]);
+	data->time_to_eat = safe_atoi(argv[3]);
+	data->time_to_sleep = safe_atoi(argv[4]);
+	if ((data->num_philos) <= 0 || ((data->time_to_die) <= 0)
+		|| ((data->time_to_eat) <= 0) || ((data->time_to_sleep) <= 0))
+		return (printf("Invalid input value\n"), 1);
+	data->must_eat_count = -1;
+	if (argc == 6)
+		data->must_eat_count = safe_atoi(argv[5]);
+	if (data->must_eat_count == 0 || data->must_eat_count < -1)
+		return (printf("Invalid number of times to eat\n"), 1);
+	data->all_alive = 1;
+	return (0);
+}
+
+int	init_data(t_data *data)
+{
+	int	i;
+
+	data->philos = malloc(data->num_philos * sizeof(t_philo));
+	data->forks = malloc(data->num_philos * sizeof(pthread_mutex_t));
+	if (!data->philos || !data->forks)
+		return (free_all(data), printf("Malloc failed\n"), 1);
+	if (pthread_mutex_init(&data->meal_lock, NULL) != 0
+		|| pthread_mutex_init(&data->write_lock, NULL) != 0
+		|| pthread_mutex_init(&data->death_lock, NULL) != 0)
+		return (free_all(data), printf("Mutex init failed\n"), 1);
+	i = -1;
+	data->start_time = get_time();
+	while (++i < data->num_philos)
+	{
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (free_all(data), printf("Mutex init failed\n"), 1);
+		data->philos[i].id = i + 1;
+		data->philos[i].eat_count = 0;
+		data->philos[i].last_meal = data->start_time;
+		data->philos[i].data = data;
+		data->philos[i].left_fork = &data->forks[i];
+		data->philos[i].right_fork = &data->forks[(i + 1) % data->num_philos];
+	}
+	return (0);
+}
+
+void	ready_set_go(t_data *data, int i)
+{
+	while (++i < data->num_philos)
+	{
+		if (pthread_create(&data->threads[i], NULL, (void *)philosopher_life,
+				&data->philos[i]) != 0)
+		{
+			pthread_mutex_lock(&data->start_lock);
+			data->simulation_started = -1;
+			pthread_mutex_unlock(&data->start_lock);
+			while (--i >= 0)
+				pthread_join(data->threads[i], NULL);
+			free_all(data);
+			printf("Thread creation failed - not enough memory\n");
+			exit(1);
+		}
+	}
+	pthread_mutex_lock(&data->start_lock);
+	while (data->ready_count < data->num_philos)
+	{
+		pthread_mutex_unlock(&data->start_lock);
+		usleep(100);
+		pthread_mutex_lock(&data->start_lock);
+	}
+	data->simulation_started = 1;
+	pthread_mutex_unlock(&data->start_lock);
+}
+
+void	start_simulation(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	data->threads = malloc(data->num_philos * sizeof(pthread_t));
+	if (!data->threads)
+	{
+		free_all(data);
+		exit(1);
+	}
+	ready_set_go(data, i);
+	i = 0;
+	while (i < data->num_philos)
+		pthread_join(data->threads[i++], NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+	int		check;
+
+	if (argc != 5 && argc != 6)
+		return (printf(USAGE, argv[0]), 1);
+	check = parse_args(&data, argc, argv);
+	if (check == 2)
+		exit(0);
+	else if (check == 1)
+		return (exit(1), 1);
+	if (init_data(&data) != 0)
+		return (1);
+	if (pthread_mutex_init(&data.start_lock, NULL) != 0)
+		return (free_all(&data), printf("Mutex init failed\n"), 1);
+	data.ready_count = 0;
+	data.simulation_started = 0;
+	start_simulation(&data);
+	free_all(&data);
+	return (0);
+}
---
+++ Philo/philo.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vagarcia <vagarcia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 17:54:09 by vagarcia          #+#    #+#             */
+/*   Updated: 2025/02/27 19:38:17 by vagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <limits.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+# define USAGE "Usage: %s num_of_philos tt_die tt_eat tt_sleep [num_must_eat]\n"
+
+typedef struct s_philo
+{
+	int				id;
+	int				eat_count;
+	long			last_meal;
+	struct s_data	*data;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}					t_philo;
+
+typedef struct s_data
+{
+	int				num_philos;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	int				must_eat_count;
+	int				all_alive;
+	long			start_time;
+	int				ready_count;
+	int				simulation_started;
+	t_philo			*philos;
+	pthread_t		*threads;
+	pthread_mutex_t	start_lock;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	write_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	death_lock;
+}					t_data;
+
+// philo.c
+int					main(int argc, char **argv);
+int					init_data(t_data *data);
+void				start_simulation(t_data *data);
+
+// philosopher.c
+void				philosopher_life(t_philo *philo);
+int					take_forks(t_philo *philo);
+void				drop_forks(t_philo *philo);
+void				eat(t_philo *philo);
+void				sleep_and_think(t_philo *philo);
+
+// utils.c
+long				get_time(void);
+int					ft_usleep(long usec, t_philo *philo);
+int					print_status(t_philo *philo, const char *status);
+int					safe_atoi(const char *str);
+void				free_all(t_data *data);
+int					parse_args(t_data *data, int argc, char **argv);
+void				take_forks_uneven(t_philo *philo);
+void				take_forks_even(t_philo *philo);
+int					check_cond(t_philo *philo);
+int					is_valid_number(char *str);
+void				wait_loop(t_philo *philo);
+
+#endif
