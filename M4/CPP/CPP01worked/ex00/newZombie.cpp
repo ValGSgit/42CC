e7dd18d94ec7288ed6a05d69cb7726c3605500f0
Valentino <entinosusini@gmail.com>
2025-06-21 16:07:19 +0200

    Delete M4/CPP directory

--- M4/CPP/CPP01worked/ex00/newZombie.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-// Creates a zombie on the HEAP and returns a pointer
-// The zombie persists until explicitly deleted
-Zombie* newZombie(std::string name) {
-	return new Zombie(name);
-}
--- M4/CPP/CPP01worked/ex00/randomChump.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-// Creates a zombie on the STACK and makes it announce itself
-// The zombie is automatically destroyed when function ends
-void randomChump(std::string name) {
-	Zombie zombie(name);
-	zombie.announce();
-	// Zombie is automatically destroyed here when going out of scope
-}
